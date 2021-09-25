7cb2e20573b043a98f0334611a91de84dad6a7fc
sukyeongh <hsk10271@gmail.com>
2021-09-25 23:21:27 +0900

    Create example3-6, 3-7.java

---
+++ 3장 함수/example3-6.java
@@ -0,0 +1,16 @@
+public class UserValidator {
+    private Cryptographer cryptographer;
+    
+    public boolean checkPassword(String userName, String password) {
+        User user = UserGateway.findByName(userName);
+        if (user != User.NULL) {
+            String codedPhrase = user.getPhraseEncodedByPassword();
+            String phrase = cryptographer.decrypt(codedPhrase, password);
+            if ("Valid Password".equals(phrase)) {
+                Session.initialize();
+                return true;
+            }
+        }
+        return false;
+    }
+}
